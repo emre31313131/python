203583dd59b2c0e0161933462b1401b90243d1e0
Emre Aygün <emreaygun630@gmail.com>
2022-12-10 19:23:38 +0300

    emr

--- asalmi.py
+++ asalmi.py
@@ -1,18 +1,20 @@
-a=int(input("bir sayı gir:"))
-asalmidir=True
+try:
+   a=int(input("bir sayı gir:"))
+   asalmidir=True
 
-if a<=1:
+   if a<=1:
     asalmidir=False
 
-for t in range(2,a):
-    if(a%t==0):
-        asalmidir=False
-        break
+   for t in range(2,a):
+        if(a%t==0):
+         asalmidir=False
+         break
 
 
-if asalmidir:
-    print("sayı asaldır ")
-else:
-    print("sayı asal değildir ")    
-
+   if asalmidir:
+      print("sayı asaldır ")
+   else:
+      print("sayı asal değildir ")    
+except ValueError:
+    print("lütfen tam sayı giriniz")
 
